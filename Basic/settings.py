a673bb71ef908350fb8817e39631b811d982e8f3
srilasya24 <lasyasxd24@gmail.com>
2016-10-17 19:18:48 -0500

    Creating templates

--- Basic/settings.py
+++ Basic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'Basic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR + '/loginapp.sqlite',
+        'USER': '',
+        'PASSSWORD':'',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
